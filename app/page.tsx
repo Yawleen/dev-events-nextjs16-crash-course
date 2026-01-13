1f0e2bd6681a77413e478b4e421f71a2d62ff3e1
Yolène <yoleneconstable@live.fr>
2026-01-13 18:43:49 +0100

    Création de la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import ExploreButton from '@/components/ExploreButton';
+import EventCard from '@/components/EventCard';
+import { events } from '../lib/constants';
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +11,16 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
+
+      <ExploreButton />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>{<EventCard {...event} />}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
